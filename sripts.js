db6e4a4738c26976192b91c298c583c2497d1d9e
JosepMarengo <168607895+JosepMarengo@users.noreply.github.com>
2024-11-23 09:55:57 -0500

    Add files via upload

---
+++ sripts.js
@@ -0,0 +1,32 @@
+//container 1
+document.getElementById('division').addEventListener('submit', function(event){event.preventDefault();
+
+const divisor1 = parseFloat(document.getElementById('divisor1').value);
+const divisor2 = parseFloat(document.getElementById('divisor2').value);
+const resultado = divisor1 / divisor2;
+
+document.getElementById('resultado1').textContent=`Resultado: ${resultado.toFixed(2)} `;
+
+});
+
+//container 2
+document.getElementById('multiplicador').addEventListener('submit',function(event){event.preventDefault();
+
+const num1 = parseFloat(document.getElementById('num1').value);
+const num2 = parseFloat(document.getElementById('num2').value);
+const resultado1 = num1 * num2;
+
+document.getElementById('resultado2').textContent=`Resultado ${resultado1.toFixed(2)}`;
+});
+
+//container 3
+
+document.getElementById('multiplicador2').addEventListener('submit',function(event){event.preventDefault()
+
+const nu1 = parseFloat(document.getElementById('nu1').value);
+const nu2 = parseFloat(document.getElementById('nu2').value);
+const resultado3 = nu1 * nu2;
+
+document.getElementById('resultado3').textContent=`Resultado ${resultado3.toFixed(2)}`;
+
+});
