a5c948ddfdc1557cde31e5e41ce594f5dad5f576
JosepMarengo <168607895+JosepMarengo@users.noreply.github.com>
2025-01-13 09:50:25 -0500

    Add files via upload

--- sripts.js
+++ sripts.js
@@ -5,7 +5,7 @@ const divisor1 = parseFloat(document.getElementById('divisor1').value);
 const divisor2 = parseFloat(document.getElementById('divisor2').value);
 const resultado = divisor1 / divisor2;
 
-document.getElementById('resultado1').textContent=`Resultado: ${resultado.toFixed(2)} `;
+document.getElementById('resultado1').textContent=`Cajas: ${resultado.toFixed(2)} `;
 
 });
 
@@ -16,7 +16,7 @@ const num1 = parseFloat(document.getElementById('num1').value);
 const num2 = parseFloat(document.getElementById('num2').value);
 const resultado1 = num1 * num2;
 
-document.getElementById('resultado2').textContent=`Resultado ${resultado1.toFixed(2)}`;
+document.getElementById('resultado2').textContent=`Precio: ${resultado1.toFixed(2)}`;
 });
 
 //container 3
@@ -27,6 +27,6 @@ const nu1 = parseFloat(document.getElementById('nu1').value);
 const nu2 = parseFloat(document.getElementById('nu2').value);
 const resultado3 = nu1 * nu2;
 
-document.getElementById('resultado3').textContent=`Resultado ${resultado3.toFixed(2)}`;
+document.getElementById('resultado3').textContent=`Total: ${resultado3.toFixed(2)}`;
 
 });
